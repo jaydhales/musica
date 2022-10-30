db0a0f5d4694245720346388fbeb56966ae0d413
James Osunkiyesi <jaydhales@gmail.com>
2022-10-30 13:06:42 +0100

    ALBUM IMAGES TO LINK

--- src/components/AlbumCard.jsx
+++ src/components/AlbumCard.jsx
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 const AlbumCard = ({ info }) => {
   return (
     <div className="album-card">
-      <img
-        src={
-          info.cover_medium || info.picture_medium || info.album.cover_medium
-        }
-        alt=""
-      />
-
       <Link to={"/" + info.type + "/" + info.id}>
+        <img
+          src={
+            info.cover_medium || info.picture_medium || info.album.cover_medium
+          }
+          alt=""
+        />
         <h3>{info.title}</h3>
       </Link>
 
