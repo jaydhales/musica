41a9c01f03e92c68fbf6c91ab3de622b53ef180f
James Osunkiyesi <jaydhales@gmail.com>
2022-11-05 05:39:58 +0100

    fixing cors issue

--- src/contexts/MusicContext.jsx
+++ src/contexts/MusicContext.jsx
@@ -19,6 +19,26 @@ const MusicContextProvider = ({ children }) => {
   const [isNavOpen, setNavOpen] = useState(true);
   const [searchResults, setSearchResults] = useState([]);
 
+  (function () {
+    var cors_api_host = "cors-anywhere.herokuapp.com";
+    var cors_api_url = "https://" + cors_api_host + "/";
+    var slice = [].slice;
+    var origin = window.location.protocol + "//" + window.location.host;
+    var open = XMLHttpRequest.prototype.open;
+    XMLHttpRequest.prototype.open = function () {
+      var args = slice.call(arguments);
+      var targetOrigin = /^https?:\/\/([^\/]+)/i.exec(args[1]);
+      if (
+        targetOrigin &&
+        targetOrigin[0].toLowerCase() !== origin &&
+        targetOrigin[1] !== cors_api_host
+      ) {
+        args[1] = cors_api_url + args[1];
+      }
+      return open.apply(this, args);
+    };
+  })();
+
   const {
     myCollections,
     addToCollections,
