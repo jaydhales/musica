f6535a562aeb84a980a7ebda82a6f70b94340e7d
James Osunkiyesi <jaydhales@gmail.com>
2022-11-17 18:11:40 +0100

    updates

--- src/routes/Collections.jsx
+++ src/routes/Collections.jsx
@@ -3,9 +3,22 @@ import { Link } from "react-router-dom";
 import { MusicContext } from "../contexts/MusicContext";
 
 import "../styles/collection.css";
+import Play from "../assets/Play-2.svg";
 
 const Collections = () => {
-  const { myCollections } = useContext(MusicContext);
+  const { myCollections, setAudioQueue, setTrackIndex } =
+    useContext(MusicContext);
+
+  const convertNum = (num) => {
+    num *= 1000;
+
+    if (num / 1000000 > 1) {
+      return (num / 1000000).toFixed(1) + "m";
+    } else if (num / 1000 > 1) {
+      return (num / 1000).toFixed(1) + "k";
+    }
+    return num;
+  };
 
   if (!myCollections) return null;
 
@@ -14,24 +27,34 @@ const Collections = () => {
   return (
     <section className="collections-sect">
       {myCollections.map(({ data }) => (
-        <Link
-          to={`/${data.type}/${data.id}`}
-          key={data.id}
-          className="collection-card group"
-        >
+        <div className="collection-card group" key={data.id}>
           <img
-            src={data.picture_medium || data.cover_medium}
+            src={data.picture_xl || data.cover_xl}
             alt=""
-            className="group-hover:scale-125"
+            className="group-hover:scale-125 coll-bg"
           />
           <div className="coll-content">
-            <div>
-              <h4>{data.title}</h4>
-              {data.artist && <p>{data.artist.name}</p>}
-              {data.creator && <p>{data.creator.name}</p>}
+            <div className="group-hover:bottom-5">
+              <Link to={`/${data.type}/${data.id}`}>
+                <p className="title">{data.title}</p>
+              </Link>
+
+              {data.artist && <p className="artist">{data.artist.name}</p>}
+              {data.creator && <p className="artist">{data.creator.name}</p>}
+              <p className="likes">{convertNum(data.fans)} likes</p>
             </div>
+            <a
+              href="#"
+              className="coll-play group-hover:bottom-5"
+              onClick={(e) => {
+                setAudioQueue(data);
+                setTrackIndex(0);
+              }}
+            >
+              <img src={Play} alt="play icon" />
+            </a>
           </div>
-        </Link>
+        </div>
       ))}
     </section>
   );
